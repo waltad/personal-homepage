b04bf206e94c665c771e26da1f521d1ecb6a7932
Tadek Waluga <tadek.waluga@gmail.com>
2023-11-06 20:15:07 +0100

    added changing text

--- src/common/themeButton/index.js
+++ src/common/themeButton/index.js
@@ -1,19 +1,17 @@
-// import { ReactComponent as ToggleOff } from "./../../images/ToggleOff-light.svg";
 import bg from "./../../images/bg.svg";
-// import toggle from "./../../images/toggle.svg";
-// import brightness from "./../../images/brightness.svg";
 import Sun from "./sun";
 import { Button, SmallText } from "./styled";
-import { toggleSun } from "./sun/sunSlice";
-import { useDispatch } from "react-redux";
+import { selectIsMoveRight, toggleSun } from "./sun/sunSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ThemeButton = () => {
-  
+  const isMoveRight = useSelector(selectIsMoveRight);
+
   const dispatch = useDispatch();
-  
+
   return (
     <Button onClick={() => dispatch(toggleSun())}>
-      <SmallText>DARK MODE OFF</SmallText>
+      <SmallText>DARK MODE {isMoveRight ? "ON" : "OFF"}</SmallText>
       <svg width="48" height="26" xmlns="http://www.w3.org/2000/svg">
         <image href={bg} x="0" y="0" width="48" height="26" />
         <Sun />
