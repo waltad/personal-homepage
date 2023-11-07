7e429bcec95913e1b39ad5456c657ee5209dfe0a
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-11-07 14:19:03 +0100

    removed unused style

--- src/common/themeButton/sun/styled.js
+++ src/common/themeButton/sun/styled.js
@@ -1,15 +1,5 @@
 import styled, { css } from "styled-components";
 
-export const StyledSvg = styled.svg`
-  transition: 0.3s;
-
-  ${({ isMoveRight }) =>
-    isMoveRight &&
-    css`
-      transform: translate(20px);
-    `}
-`;
-
 export const StyledImg = styled.image`
   transition: 0.3s;
 
