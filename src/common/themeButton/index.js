7d9c7ddabd57fc987ccdb0c4fecd341719efd443
Tadek Waluga <tadek.waluga@gmail.com>
2023-11-07 20:13:57 +0100

    replacing sunSlice with themeSlice

--- src/common/themeButton/index.js
+++ src/common/themeButton/index.js
@@ -1,22 +1,25 @@
 import bg from "./../../images/bg.svg";
 import Sun from "./sun";
-import { Button, SmallText } from "./styled";
-import { selectIsMoveRight, toggleSun } from "./sun/sunSlice";
+import { Wrapper, Button, SmallText } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
+import { selectIsThemeDark, toggleTheme } from "./themeSlice";
+import { Wrapper } from "../section/styled";
 
 const ThemeButton = () => {
-  const isMoveRight = useSelector(selectIsMoveRight);
+  const isThemeDark = useSelector(selectIsThemeDark);
 
   const dispatch = useDispatch();
 
   return (
-    <Button onClick={() => dispatch(toggleSun())}>
-      <SmallText>DARK MODE {isMoveRight ? "ON" : "OFF"}</SmallText>
-      <svg width="48" height="26" xmlns="http://www.w3.org/2000/svg">
-        <image href={bg} x="0" y="0" width="48" height="26" />
-        <Sun />
-      </svg>
-    </Button>
+    <Wrapper>
+      <Button onClick={() => dispatch(toggleTheme())}>
+        <SmallText>DARK MODE {isThemeDark ? "ON" : "OFF"}</SmallText>
+        <svg width="48" height="26" xmlns="http://www.w3.org/2000/svg">
+          <image href={bg} x="0" y="0" width="48" height="26" />
+          <Sun />
+        </svg>
+      </Button>
+    </Wrapper>
   );
 };
 
--- src/common/themeButton/sun/index.js
+++ src/common/themeButton/sun/index.js
@@ -2,15 +2,15 @@ import toggle from "./../../../images/toggle.svg";
 import brightness from "./../../../images/brightness.svg";
 import { StyledImg } from "./styled";
 import { useSelector } from "react-redux";
-import { selectIsMoveRight } from "./sunSlice";
+import { selectIsThemeDark } from "../themeSlice";
 
 const Sun = () => {
-  const isMoveRight = useSelector(selectIsMoveRight);
+  const isThemeDark = useSelector(selectIsThemeDark);
   
   return (
   <svg width="48" height="26" xmlns="http://www.w3.org/2000/svg">
-    <StyledImg isMoveRight={isMoveRight} href={toggle} x="3" y="3" />
-    <StyledImg isMoveRight={isMoveRight} href={brightness} x="6" y="6" />
+    <StyledImg moveToRight={isThemeDark} href={toggle} x="3" y="3" />
+    <StyledImg moveToRight={isThemeDark} href={brightness} x="6" y="6" />
   </svg>
 )};
 
