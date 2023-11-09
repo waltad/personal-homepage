6ddbd989a2af7068d7dd849b2d035635c8066aef
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-11-09 14:43:06 +0100

    colors added

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,18 @@
 export const theme = {
   colors: {
+    blueRuin: "#0366D6",
+    chainGangGrey: "#6E7E91",
+    coldMorninig: "#E5E5E5",
+    darkGrey: rgba(54, 54, 54, 0.72),
+    nero: "#252525",
+    poolWater: "#2188FF",
+    sparklingFrost: rgba(209, 213, 218, 0.3), //#d1d5da
     white: "#FFFFFF",
     washMe: "#FBFBFE",
-    chainGangGrey: "#6E7E91",
   },
 
   breakpoints: {
 
   },
+  boxShadow: "0px 16px 58px 0px rgba(9, 10, 51, 0.03), 0px -2px 50px 0px rgba(9, 10, 51, 0.02)",
 };
