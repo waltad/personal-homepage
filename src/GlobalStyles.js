50949b8409bc976589d43eba4041c41365202947
Tadek Waluga <tadek.waluga@gmail.com>
2024-01-13 19:28:43 +0100

    updated GlobalStyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -13,6 +13,17 @@ export const GlobalStyles = createGlobalStyle`
 
     body {
       font-family: 'Inter', sans-serif;
-      margin: 0;
+      transition: background 0.3s;
+      background: ${({ theme }) => theme.colors.site.background};
+      color: ${({ theme }) => theme.colors.site.text};
+      font-size: 18px;
+      letter-spacing: 0.05em;
+      word-break: break-word;
+      overflow-y: scroll;
+      padding-bottom: 108px;
+
+      @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        padding-bottom: 32px;
+      }
     }
 `;
