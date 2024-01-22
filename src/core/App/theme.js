7ec61738698088683918c13de26a8384adae2591
Tadek Waluga <tadek.waluga@gmail.com>
2024-01-22 20:00:28 +0100

    update theme

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,12 +1,15 @@
 const colorNames = {
   antiFlashWhite: "F2F3F4",
   blueRuin: "#0366D6",
+  blueTuna: "#6D93BE",
   chainGangGrey: "#6E7E91",
   coldMorning: "#E5E5E5",
   darkGrey: "rgba(54, 54, 54, 0.72)",
   nero: "#252525",
+  offBlack: "#313131",
   poolWater: "#2188FF",
   saltyIce: "CDE0F7",
+  scarabaeusSacer: "#414141",
   scenicWater: "8CC2FF",
   sparklingFrost: "rgba(209, 213, 218, 0.3)", //#d1d5da
   washMe: "#FBFBFE",
@@ -39,14 +42,14 @@ export const themeLight = {
       bordre: colorNames.sparklingFrost,
       shadow: colorNames.scenicWater,
     },
-    boxBackground: colorNames.white,
+    boxBackground: colorNames.darkGrey,
     headerline: colorNames.coldMorning,
     title: {
       border: colorNames.antiFlashWhite,
       borderHover: colorNames.saltyIce,
       header: colorNames.blueRuin,
     },
-    themeSwitch: {
+    themeButton: {
       background: colorNames.coldMorning,
       icon: colorNames.white,
     },
@@ -56,26 +59,26 @@ export const themeLight = {
 export const themeDark = {
   ...common,
   colors: {
-    primary: colorNames.blueRuin,
-    textPrimary: colorNames.nero,
+    primary: colorNames.poolWater,
+    textPrimary: colorNames.white,
     site: {
-      background: colorNames.washMe,
-      text: colorNames.chainGangGrey,
+      background: colorNames.nero,
+      text: colorNames.white,
     },
     buttonLink: {
       text: colorNames.white,
       bordre: colorNames.sparklingFrost,
-      shadow: colorNames.scenicWater,
+      shadow: colorNames.blueTuna,
     },
-    boxBackground: colorNames.white,
+    boxBackground: colorNames.darkGrey,
     headerline: colorNames.coldMorning,
     title: {
       border: colorNames.antiFlashWhite,
       borderHover: colorNames.saltyIce,
       header: colorNames.blueRuin,
     },
-    themeSwitch: {
-      background: colorNames.coldMorning,
+    themeButton: {
+      background: colorNames.darkGrey,
       icon: colorNames.white,
     },
   },
