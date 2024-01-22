13b832826e87c5fc322b6c73728f15fd2a17c5ae
Tadek Waluga <tadek.waluga@gmail.com>
2024-01-22 19:57:46 +0100

    moved store to App folder

---
+++ src/core/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "../common/themeButton/themeSlice";
+
+const store = configureStore ({
+  reducer: {
+    theme: themeReducer,
+  },
+});
+
+export default store;
